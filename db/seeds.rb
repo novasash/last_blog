064456db0b19ba9c87818abeaaf550c94002b7a9
novasash <17441942+novasash@users.noreply.github.com>
2025-02-17 01:15:56 -0600

    Add seed data for posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+12.times do
+  Post.create(
+    title: Faker::Hipster.sentence,
+    content: Faker::Hipster.paragraph_by_chars(characters: 2560, supplemental: false)
+  )
+end
